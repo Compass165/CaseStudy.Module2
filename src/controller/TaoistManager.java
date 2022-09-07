8203b622bab0115c687bb0fab1712413ef34fc70
Compass <ndphong165@gmail.com>
2022-09-07 23:17:27 +0700

    Done controller

--- src/controller/TaoistManager.java
+++ src/controller/TaoistManager.java
@@ -1,19 +1,39 @@
 package controller;
 
-import model.LunisolarTools;
+import model.Taoist;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class TaoistManager  {
+public class TaoistManager implements model.Imanager {
 
-    public int checkIndex(List arr, String toolsID) {
-        List<LunisolarTools> lunisolarTools = (List<LunisolarTools>) arr;
+    @Override
+    public int checkIndex(List arr, String taoistID) {
+        List<Taoist> taoistList = (List<Taoist>) arr;
         int index = 0;
         for (int i = 0; i < arr.size(); i++) {
-            if(toolsID.equals(lunisolarTools.get(i).getToolsID())) {
+            if (taoistID.equals(taoistList.get(i).getTaoistID())) {
                 index = i;
             }
         } return index;
     }
 
+    @Override
+    public Object search(List arr, String name) {
+        List<Taoist> taoistList = arr;
+        int index = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (name.equals(taoistList.get(i).getName())) {
+                index = i;
+            }
+        } return taoistList.get(index);
+    }
+
+    @Override
+    public boolean validate(String regex, String input) {
+        Pattern pattern= Pattern.compile(regex);
+        Matcher matcher= pattern.matcher(input);
+        return matcher.matches();
+    }
 }
